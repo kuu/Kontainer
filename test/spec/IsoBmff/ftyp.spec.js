3d39899fe4d025deca5a7f9a6aca75266d8f40fc
Kuu Miyazaki <miyazaqui@gmail.com>
2015-05-02 02:58:21 +0900

    Add a test case for FileTypeBox.

--- test/spec/IsoBmff/ftyp.spec.js
+++ test/spec/IsoBmff/ftyp.spec.js
@@ -6,6 +6,14 @@ describe('FileTypeBox', function () {
         102, 116, 121, 112, // type='ftyp'
         105, 115, 111, 109, // major_brand='isom'
         0, 0, 0, 0 // minor_version=0
+      ],
+      avc1Value = [
+        0, 0, 0, 24, // size=24
+        102, 116, 121, 112, // type='ftyp'
+        97, 118, 99, 49, // major_brand='avc1'
+        0, 0, 0, 2, // minor_version=2
+        105, 115, 111, 109, // compatible_brands[0]='isom'
+        105, 115, 111, 50 // compatible_brands[1]='iso2'
       ];
 
   it('can be initialized with the default values', function () {
@@ -14,6 +22,15 @@ describe('FileTypeBox', function () {
     expect(array.length).toBe(defaultValue.length);
     for (var i = 0, il = array.length; i < il; i++) {
       expect(array[i]).toBe(defaultValue[i]);
+    }
+  });
+
+  it('can be initialized with the specified values', function () {
+    var buffer = Kontainer.renderToArrayBuffer(IsoBmff.createElement('ftyp', {majorBrand: 'avc1', minorVersion: 2, compatibleBrands:['isom', 'iso2']}));
+    var array = new Uint8Array(buffer);
+    expect(array.length).toBe(avc1Value.length);
+    for (var i = 0, il = array.length; i < il; i++) {
+      expect(array[i]).toBe(avc1Value[i]);
     }
   });
 });
